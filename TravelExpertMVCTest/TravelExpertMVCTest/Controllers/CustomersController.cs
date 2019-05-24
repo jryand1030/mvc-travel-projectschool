1e698c61e106ca0ec25b2a72e578aa6eea3eec2b
jryand1030 <40072313+jryand1030@users.noreply.github.com>
2019-05-23 18:30:15 -0600

    finalVersionOfProject

---
+++ TravelExpertMVCTest/TravelExpertMVCTest/Controllers/CustomersController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TravelExpertMVCTest.Models;
+
+namespace TravelExpertMVCTest.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private TravelExpertsEntities db = new TravelExpertsEntities();
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            int custId = (int) Session["custId"];
+            Customer customer = db.Customers.Find(custId);
+            return View(customer);
+        }
+
+  
+        // GET: Customers/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserId = new SelectList(db.AspNetUsers, "Id", "Email");
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CustomerId,CustFirstName,CustLastName,CustAddress,CustCity,CustProv,CustPostal,CustCountry,CustHomePhone,CustBusPhone,CustEmail,AgentId,UserId")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                customer.UserId = User.Identity.GetUserId();
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                Session["custId"]= customer.CustomerId;
+                return RedirectToAction("Customer_Packages", "Home");
+            }
+
+            ViewBag.UserId = new SelectList(db.AspNetUsers, "Id", "Email", customer.UserId);
+            return View(customer);
+        }
+
+        // GET: Customers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserId = new SelectList(db.AspNetUsers, "Id", "Email", customer.UserId);
+            return View(customer);
+        }
+
+        // POST: Customers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CustomerId,CustFirstName,CustLastName,CustAddress,CustCity,CustProv,CustPostal,CustCountry,CustHomePhone,CustBusPhone,CustEmail,AgentId,UserId")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserId = new SelectList(db.AspNetUsers, "Id", "Email", customer.UserId);
+            return View(customer);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TravelExpertMVCTest/TravelExpertMVCTest/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelExpertMVCTest.Models;
+
+namespace TravelExpertMVCTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private TravelExpertsEntities db = new TravelExpertsEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Packages()
+        {
+            List<Package> packageList = db.Packages.ToList();
+            ViewBag.Message = "Your travel package page.";
+
+            return View(packageList);
+        }
+
+        public ActionResult Customer_Packages()
+        {
+            if (Session["custId"] != null)
+            {
+                int custId = (int)Session["custId"];
+                var packageList = db.Customers_Packages.Where(x => x.Customer.CustomerId == custId).ToList();
+                ViewBag.Message = "Your travel package page.";
+
+                return View(packageList);
+            }
+            else
+            {
+                return RedirectToAction("Login", "Account");
+            }
+        }
+
+        public ActionResult Buy(int id)
+        {
+            Customers_Packages custPkg = new Customers_Packages();
+            if (Session["custId"] !=null)
+            {
+                int custId = (int)Session["custId"];
+                custPkg.CustomerId = custId;
+                custPkg.PackageId = id;
+                db.Customers_Packages.Add(custPkg);
+                db.SaveChangesAsync();
+                return RedirectToAction("Customer_Packages", "Home");
+            }
+            else
+            {  
+                return RedirectToAction("Login", "Account");
+            }
+            
+        }
+
+        public ActionResult Remove(int id)
+        {
+            Customers_Packages custPkg = db.Customers_Packages.Find(id);
+            db.Customers_Packages.Remove(custPkg);
+            db.SaveChangesAsync();
+            return RedirectToAction("Customer_Packages", "Home");
+        }
+    }
+}
